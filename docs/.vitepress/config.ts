98a685d785936b9ca6a5d4db1c3a87feca0f6142
Waipok Fu <fwbfwb00@icloud.com>
2025-03-27 12:26:20 -0700

    trigger deploy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Site of Waipok Fu',
   description: '这是一个使用 VitePress 搭建的个人网站',
-  base: '/Waipok-Fu.github.io/',
+  base: '/',
 })
