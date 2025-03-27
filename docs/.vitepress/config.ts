d5d71a56f35da74e70971fb42868af21dcea43e5
Waipok Fu <fwbfwb00@icloud.com>
2025-03-26 23:41:03 -0700

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vitepress'
 
-// https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+  title: 'Site of Waipok Fu',
+  description: '这是一个使用 VitePress 搭建的个人网站',
+  base: '/', // ⚠️ 这是关键配置
+})
